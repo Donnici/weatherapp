cd869eaf3bed5fdd01899debdf4c7180fefa2109
Bruno Donnici <yep.dias@gmail.com>
2021-02-09 17:14:58 -0300

    Inicio da criação do README e remoção da credencial

--- src/config/openweather.ts
+++ src/config/openweather.ts
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-export const API_KEY = 'a249abe8903480562fae04453078dae8';
-// export const API_KEY = process.env.REACT_APP_WEATHER_KEY;
+export const API_KEY = process.env.REACT_APP_WEATHER_KEY;
 
-// ?lat={lat}&lon={lon}&appid={API key}
 export const ApiWeather = axios.create({
 	baseURL: 'https://api.openweathermap.org/data/2.5/weather',
 	timeout: 4000
